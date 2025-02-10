ad81e11e27300ea9d6152d6075a5858f6ab50c29
coderVi <mr.asmtylmz@gmail.com>
2025-02-10 03:11:23 +0300

    New Project Added

---
+++ AspLearnProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspLearnProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
